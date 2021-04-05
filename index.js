eb18dd90f566aa03f59f6724f4c9f5c4746a728b
Bogdan Shevchuk <bodjasheva@gmail.com>
2021-04-05 17:20:24 +0300

    Added required features to bot

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
 const { Telegraf } = require("telegraf")
+
+var log4js = require('log4js');
+var logger = log4js.getLogger();
 //const { express } = require("express")
 
 //const app = express()
 const bot = new Telegraf("1737603973:AAGKTsXPZFRsOyry5VzgqgYHZwADzYFRhQs")
-
 const port = 3000
+const decisions = ["Да", "Нет"]
 
-
-app.get('/', function(req, res) {
-  res.send('I\'m alive');
-});
 
 bot.start((ctx) => ctx.reply('Здраститя'))
 
-bot.on('text', (ctx)=>{
-	if(ctx.message.text.includes(""))
+
+
+bot.command('/decide', (ctx)=>{
+
+	let msgId = ctx.message.message_id
+	let decision = getDecision()
+
+	ctx.reply(decision, {reply_to_message_id: msgId})
 })
 
-bot.command('decide', (ctx)=>{
 
-})
+function getDecision(){
+	let result = Math.random() > 0.5 ? decisions[1] : decisions[0]
+	return result
+}
+
+bot.launch()
 
 /*
 app.listen(port, () => {
